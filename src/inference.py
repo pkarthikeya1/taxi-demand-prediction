abd962b2c94c88adf47213005e4d51ba3be3f10e
karthikeya pervela <69100096+pkarthikeya1@users.noreply.github.com>
2024-07-22 13:47:52 +0530

    update inferene.py

--- src/inference.py
+++ src/inference.py
@@ -44,7 +44,7 @@ def load_batch_of_features_from_feature_store(
 
     # read time-series data from the feature store 
 
-    fetch_data_from = current_date - timedelta(days=4)
+    fetch_data_from = current_date - timedelta(days=28)
     fetch_data_to = current_date - timedelta(hours=1)
     print(f"fetching data from {fetch_data_from} to {fetch_data_to}")
 
@@ -57,16 +57,24 @@ def load_batch_of_features_from_feature_store(
         start_time=fetch_data_from - timedelta(days=1),
         end_time=fetch_data_to + timedelta(days=1)
     )
-
+    # filter data to the time period we are interested in
+    # pickup_ts_from = int(fetch_data_from.timestamp() * 1000)
+    # pickup_ts_to = int(fetch_data_to.timestamp() * 1000)
     ts_data = ts_data[ts_data.pickup_hour.between(fetch_data_from, fetch_data_to)]
-
-    # validate we are not missing data in the feature store
-    location_ids = ts_data['pickup_location_id'].unique()
-    assert len(ts_data) == n_features*len(location_ids), "Time-series data is not complete"
 
     #sort data by location and time
 
     ts_data.sort_values(by = ['pickup_location_id', 'pickup_hour'], inplace =True)
+
+
+    # validate we are not missing data in the feature store
+    location_ids = ts_data['pickup_location_id'].unique()
+    assert len(ts_data) == n_features*len(location_ids), \
+        "Time-series data is not complete"
+
+    
+
+    
     # print(f"{ts_data=}")
 
     # transpose time-series data as a feature vector, for each 'location_id'
@@ -88,7 +96,8 @@ def load_batch_of_features_from_feature_store(
 
     features["pickup_hour"] = current_date
     features["pickup_location_id"] = location_ids
-    
+    features.sort_values(by=['pickup_location_id'], inplace=True)
+
     return features
     
 def load_model_from_registry():
@@ -111,50 +120,50 @@ def load_model_from_registry():
     return model
 
 
-def load_predictions_from_store(
-    from_pickup_hour: datetime,
-    to_pickup_hour: datetime
-    ) -> pd.DataFrame:
-    """
-    Connects to the feature store and retrieves model predictions for all
-    `pickup_location_id`s and for the time period from `from_pickup_hour`
-    to `to_pickup_hour`
+# def load_predictions_from_store(
+#     from_pickup_hour: datetime,
+#     to_pickup_hour: datetime
+#     ) -> pd.DataFrame:
+#     """
+#     Connects to the feature store and retrieves model predictions for all
+#     `pickup_location_id`s and for the time period from `from_pickup_hour`
+#     to `to_pickup_hour`
 
-    Args:
-        from_pickup_hour (datetime): min datetime (rounded hour) for which we want to get
-        predictions
+#     Args:
+#         from_pickup_hour (datetime): min datetime (rounded hour) for which we want to get
+#         predictions
 
-        to_pickup_hour (datetime): max datetime (rounded hour) for which we want to get
-        predictions
+#         to_pickup_hour (datetime): max datetime (rounded hour) for which we want to get
+#         predictions
 
-    Returns:
-        pd.DataFrame: 3 columns:
-            - `pickup_location_id`
-            - `predicted_demand`
-            - `pickup_hour`
-    """
-    from src.config import FEATURE_VIEW_PREDICTIONS_METADATA
-    from src.feature_store_api import get_or_create_feature_view
+#     Returns:
+#         pd.DataFrame: 3 columns:
+#             - `pickup_location_id`
+#             - `predicted_demand`
+#             - `pickup_hour`
+#     """
+#     from src.config import FEATURE_VIEW_PREDICTIONS_METADATA
+#     from src.feature_store_api import get_or_create_feature_view
 
-    # get pointer to the feature view
-    predictions_fv = get_or_create_feature_view(FEATURE_VIEW_PREDICTIONS_METADATA)
+#     # get pointer to the feature view
+#     predictions_fv = get_or_create_feature_view(FEATURE_VIEW_PREDICTIONS_METADATA)
 
-    # get data from the feature view
-    print(f'Fetching predictions for `pickup_hours` between {from_pickup_hour}  and {to_pickup_hour}')
-    predictions = predictions_fv.get_batch_data(
-        start_time=from_pickup_hour - timedelta(days=1),
-        end_time=to_pickup_hour + timedelta(days=1)
-    )
+#     # get data from the feature view
+#     print(f'Fetching predictions for `pickup_hours` between {from_pickup_hour}  and {to_pickup_hour}')
+#     predictions = predictions_fv.get_batch_data(
+#         start_time=from_pickup_hour - timedelta(days=1),
+#         end_time=to_pickup_hour + timedelta(days=1)
+#     )
     
-    # make sure datetimes are UTC aware
-    predictions['pickup_hour'] = pd.to_datetime(predictions['pickup_hour'], utc=True)
-    from_pickup_hour = pd.to_datetime(from_pickup_hour, utc=True)
-    to_pickup_hour = pd.to_datetime(to_pickup_hour, utc=True)
+#     # make sure datetimes are UTC aware
+#     predictions['pickup_hour'] = pd.to_datetime(predictions['pickup_hour'], utc=True)
+#     from_pickup_hour = pd.to_datetime(from_pickup_hour, utc=True)
+#     to_pickup_hour = pd.to_datetime(to_pickup_hour, utc=True)
 
-    # make sure we keep only the range we want
-    predictions = predictions[predictions.pickup_hour.between(from_pickup_hour, to_pickup_hour)]
+#     # make sure we keep only the range we want
+#     predictions = predictions[predictions.pickup_hour.between(from_pickup_hour, to_pickup_hour)]
 
-    # sort by `pick_up_hour` and `pickup_location_id`
-    predictions.sort_values(by=['pickup_hour', 'pickup_location_id'], inplace=True)
+#     # sort by `pick_up_hour` and `pickup_location_id`
+#     predictions.sort_values(by=['pickup_hour', 'pickup_location_id'], inplace=True)
 
-    return predictions
+#     return predictions
